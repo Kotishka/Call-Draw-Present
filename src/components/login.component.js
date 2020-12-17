17225bf0e5a46b0a2223f588c2ada95bc9e7e6c9
Yaroslava Boyko <ysboyko@gmail.com>
2020-12-17 18:32:28 -0500

    Adding Amplify backend

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React from 'react';
+import "../App.css";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-export default class CreateTodo extends Component {
-    render() {
+function CreateTodo() {
         return (
-            <div>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
+            <AmplifySignOut />
         )
-    }
 }
+
+export default withAuthenticator(CreateTodo);
